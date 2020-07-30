543410f83dc036366200bbaa253caa7dad854745
Guy Chaplin <guy.chaplin@googlemail.com>
2020-07-30 10:35:07 +0100

    added new page function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from markdown2 import markdown
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 
 
 from . import util
+
+
+class NewPageForm(forms.Form):
+    entry_title = forms.CharField(label="Title")
+    entry_body = forms.CharField(widget=forms.Textarea)
+
+
 # index page shows the list of encyclopedia entries
 
 
@@ -46,4 +54,27 @@ def search(request):
                 substring_entries.append(entry)
         return render(request, "encyclopedia/search_results.html", {
             "substring_entries": substring_entries
+        })
+
+
+def new_page(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            entry_title = form.cleaned_data["entry_title"]
+            entry_body = form.cleaned_data["entry_body"]
+            if entry_title in util.list_entries():
+                return render(request, "encyclopedia/error.html", {
+                    "message": "Title Not Available"
+                })
+            else:
+                util.save_entry(entry_title, entry_body)
+                return HttpResponseRedirect(reverse('entry', args=(entry_title,)))
+        else:
+            return render(request, "encyclopedia/new_page.html", {
+                "form": form
+            })
+    else:
+        return render(request, "encyclopedia/new_page.html", {
+            "form": NewPageForm()
         })
